e0e8a55e765be70b8a9e23212dacead954a10074
Myckol Fontes <myckol.ti@gmail.com>
2022-06-19 17:38:48 -0300

    login, logout e refresh JWT

--- app/Http/Controllers/LoginJwtController.php
+++ app/Http/Controllers/LoginJwtController.php
@@ -8,12 +8,27 @@ use App\Api\ApiMessages;
 class LoginJwtController extends Controller
 {
     public function login(Request $request){
+
         $credentials = $request->all(['email', 'password']);
 
         if(!$token = auth('api')->attempt($credentials)){
             $message = new ApiMessages('Unauthorized');
             return response()->json($message->getMessage(), 401);
         }
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function logout(){
+        auth('api')->logout();
+
+        return response()->json(['message' => 'Logout successfully!!'], 200);
+    }
+
+    public function refresh(){
+        $token = auth('api')->refresh();
 
         return response()->json([
             'token' => $token
--- routes/api.php
+++ routes/api.php
@@ -27,22 +27,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function(){
 
     Route::post('login', [LoginJwtController::class, 'login'])->name('login');
+    Route::get('logout', [LoginJwtController::class, 'logout'])->name('logout');
+    Route::get('refresh', [LoginJwtController::class, 'refresh'])->name('refresh');
 
-    Route::name('real_states.')->group(function(){
-        Route::resource('real-states', 'App\Http\Controllers\RealStateController');
-    });
+    Route::group(['middleware' => ['jwt.auth']], function(){
+        Route::name('real_states.')->group(function(){
+            Route::resource('real-states', 'App\Http\Controllers\RealStateController');
+        });
 
-    Route::name('users.')->group(function(){
-        Route::resource('users', 'App\Http\Controllers\UserController');
-    });
+        Route::name('users.')->group(function(){
+            Route::resource('users', 'App\Http\Controllers\UserController');
+        });
 
-    Route::name('categories.')->group(function(){
-        Route::get('categories/{id}/real-states', [CategoryController::class, 'realState']);
-        Route::resource('categories', 'App\Http\Controllers\CategoryController');
-    });
+        Route::name('categories.')->group(function(){
+            Route::get('categories/{id}/real-states', [CategoryController::class, 'realState']);
+            Route::resource('categories', 'App\Http\Controllers\CategoryController');
+        });
 
-    Route::name('photos.')->prefix('photos')->group(function(){
-        Route::delete('/{id}', [RealStatePhotoController::class, 'remove'])->name('delete');
-        Route::put('/set-thumb/{photoId}/{realStateId}', [RealStatePhotoController::class, 'setThumb'])->name('delete');
+        Route::name('photos.')->prefix('photos')->group(function(){
+            Route::delete('/{id}', [RealStatePhotoController::class, 'remove'])->name('delete');
+            Route::put('/set-thumb/{photoId}/{realStateId}', [RealStatePhotoController::class, 'setThumb'])->name('delete');
+        });
     });
 });
